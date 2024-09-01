61f7c6b9e34ee070e0a66b82147fb6b8044dee74
ragami <64999021+ragami@users.noreply.github.com>
2024-09-01 11:33:04 +1000

    Add files via upload

---
+++ lab_5/shoppingform.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Web application development" />
+    <meta name="keywords" content="PHP" />
+    <meta name="author" content="Lorraine Becker" />
+    <title>Week 5 - Task 1</title>
+</head>
+
+<body>
+    <h1>Web Programming - Lab 5</h1>
+    <hr>
+    <form action="shoppingsave.php" method="POST">
+        <label for="name">Item Name</label>
+        <input type="text" name="name" id="name">
+        <br>
+        <label for="quantity">Quantity</label>
+        <input type="text" name="quantity" id="quantity">
+        <br>
+        <input type="submit" value="Save">
+    </form>
+</body>
+
+</html>
