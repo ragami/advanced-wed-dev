e5bcac8c9cd3ec5ced5c0be3bbddae3b09aa734f
ragami <64999021+ragami@users.noreply.github.com>
2024-09-06 19:44:14 +1000

    Update index.php

--- index.php
+++ index.php
@@ -2,32 +2,59 @@
 <html lang="en">
 
 <head>
+    <!-- Meta tags for character set, description, and keywords -->
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="description" content="Assignment 1" />
     <meta name="keywords" content="Advanced Web Development" />
+    
+    <!-- Page title -->
     <title>Assignment 1</title>
+    
+    <!-- Link to stylesheet -->
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div class="navbar">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a class="active" href="postjobform.php">Post Job</a></li>
-            <li><a href="searchjobform.php">Job Search</a></li>
-            <li><a href="about.php">About</a></li>
-        </ul>
+    <!-- Header section with navigation -->
+    <header>
+        <nav class="navbar">
+            <ul>
+                <li><a class="active" href="index.php">Home</a></li>
+                <li><a href="postjobform.php">Post a Job</a></li>
+                <li><a href="searchjobform.php">Job Search</a></li>
+                <li><a href="about.php">About</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <!-- Main heading -->
+    <br>
+    <h1 class="page-title">Job Vacancy Posting System</h1>
+    
+    <!-- Student information box -->
+    <div class="box student-info">
+        <p>
+            Name: Lorraine Becker<br>
+            Student ID: 104584773<br>
+            Email: <a href="mailto:104584773@student.swin.edu.au">104584773@student.swin.edu.au</a>
+        </p>
     </div>
-    <h1>Job Vacancy Posting System</h1>
-    <p> Name: Lorraine Becker<br>
-        Student ID: 104584773<br>
-        Email: <a href="mailto: 104584773@student.swin.edu.au">104584773@student.swin.edu.au</a>
-    </p>
-    <p>
-        I declare that this assignment is my individual work. 
-        I have not worked collaboratively, nor have I copied from any other student's work or from any other source. </p>
-    <p><a href="postjobform.php">Post a job vacancy</a></p>
-    <p><a href="searchjobform.php">Search for a job vacancy</a></p>
-    <p><a href="about.php">About the Assignment </a></p>
-
+    
+    <!-- Declaration box -->
+    <div class="box declaration">
+        <p>
+            I declare that this assignment is my individual work. 
+            I have not worked collaboratively, nor have I copied from any other student's work or from any other source.
+        </p>
+    </div>
+    
+    <!-- Links box -->
+    <div class="box links">
+        <p><a href="postjobform.php">Post a job vacancy</a></p>
+        <p><a href="searchjobform.php">Search for a job vacancy</a></p>
+        <p><a href="about.php">About the Assignment</a></p>
+    </div>
 </body>
+
 </html>
+
