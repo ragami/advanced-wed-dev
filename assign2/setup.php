e0a42a6b2f00f6bf7d8193236191c35f6f7d4291
ragami <64999021+ragami@users.noreply.github.com>
2025-04-09 11:49:57 +1000

    Update setup.php

--- assign2/setup.php
+++ assign2/setup.php
@@ -2,8 +2,8 @@
 //Database connection details
 $host = "feenix-mariadb.swin.edu.au";
 $user = "s104584773";
-$pass = "151004";
-$db = "s104584773_db";
+$pass = .....
+$db = .....
 
 //Assignment tables 
 $friends = "friends";
